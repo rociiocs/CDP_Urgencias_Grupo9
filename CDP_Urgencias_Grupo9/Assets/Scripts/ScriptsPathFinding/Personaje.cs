b912db001823d2a9f2fac6370f330d8188e7236c
CelMW <49288605+CelMW@users.noreply.github.com>
2020-12-17 16:00:51 +0100

    PathFinding Añadido
    En la carpeta Scripts he añadido los scripts "UiManagerPruebaPathFinding" que es el que gestiona lo de los botones para mandar al bichin a diferentes sitios, y el script "personaje", que tiene un método "GoTo(Vector3 position)" que lo lleva hasta el punto indicado. En la escena he creado un objeto vacio cuyos hijos son todos los gameobjects que he usado para las pruebas

---
+++ CDP_Urgencias_Grupo9/Assets/Scripts/ScriptsPathFinding/Personaje.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Personaje : MonoBehaviour
+{
+    NavMeshAgent myAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    public void GoTo(Vector3 position)
+    {
+        myAgent.SetDestination(position);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CDP_Urgencias_Grupo9/Assets/Scripts/ScriptsPathFinding/UIManagerPruebaPathFinding.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerPruebaPathFinding : MonoBehaviour
+{
+    List<Personaje> personajesEscena;
+    public Button pos1, pos2, pos3;
+    public Transform target1, target2, target3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        personajesEscena = new List<Personaje>();
+        personajesEscena.AddRange(FindObjectsOfType<Personaje>());
+        pos1.onClick.AddListener(() => SetDestination(target1.position));
+        pos2.onClick.AddListener(() => SetDestination(target2.position));
+        pos3.onClick.AddListener(() => SetDestination(target3.position));
+    }
+    public void SetDestination(Vector3 pos)
+    {
+        foreach(Personaje p in personajesEscena)
+        {
+            p.GoTo(pos);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
