b0d6db460895ed9bfc10e547307feebd705b4142
rociiocs <49315467+rociiocs@users.noreply.github.com>
2020-12-19 18:16:07 +0100

    AñadidoPersonaje
    Personaje con texturas y algunas animaciones, falta adaptarlas al path finding

--- CDP_Urgencias_Grupo9/Assets/Scripts/ScriptsPathFinding/Personaje.cs
+++ CDP_Urgencias_Grupo9/Assets/Scripts/ScriptsPathFinding/Personaje.cs
@@ -6,16 +6,18 @@ using UnityEngine.AI;
 public class Personaje : MonoBehaviour
 {
     NavMeshAgent myAgent;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponentInChildren<Animator>();
         myAgent = GetComponent<NavMeshAgent>();
-        
     }
 
     public void GoTo(Vector3 position)
     {
         myAgent.SetDestination(position);
+        animator.SetBool("Walking", true);
     }
     // Update is called once per frame
     void Update()
