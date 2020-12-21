bf7f5b7fecd489a05918ba06a9e0cb3f43e3bb0f
CelMW <49288605+CelMW@users.noreply.github.com>
2020-12-21 11:08:15 +0100

    Solucionado exit time del animatorcontroller, y cuando el personaje llega al destino se para

--- CDP_Urgencias_Grupo9/Assets/Scripts/ScriptsPathFinding/Personaje.cs
+++ CDP_Urgencias_Grupo9/Assets/Scripts/ScriptsPathFinding/Personaje.cs
@@ -7,21 +7,41 @@ public class Personaje : MonoBehaviour
 {
     NavMeshAgent myAgent;
     Animator animator;
-    // Start is called before the first frame update
+    bool andando = false;
+    float epsilon = 0.05f;
+
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
         myAgent = GetComponent<NavMeshAgent>();
+       
     }
 
     public void GoTo(Vector3 position)
     {
         myAgent.SetDestination(position);
         animator.SetBool("Walking", true);
+        StartCoroutine(tiempoEspera());
     }
-    // Update is called once per frame
+
     void Update()
     {
-        
+        //Cuando llega al destino, la animacion se para
+        if ((AproximadamenteCero(myAgent.remainingDistance))&& (andando)){
+
+            andando = false;
+            animator.SetBool("Walking", false);
+            myAgent.Stop();
+        }
+    }
+    private bool AproximadamenteCero(float value){
+
+        return value <= epsilon;
+
+    }
+    IEnumerator tiempoEspera()// es necesario porque por alguna razon entra en la condicion del update justo despues de comenzar a caminar
+    {
+        yield return new WaitForSeconds(0.5f);
+        andando = true;
     }
 }
